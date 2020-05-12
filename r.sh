e1c8f322e33c925e73aed3aa6337e383cb788c61
Dev Patel <dvptl68@gmail.com>
2020-05-12 13:41:55 -0400

    Implement script to compile and run program

---
+++ r.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo
+echo "Compiling..."
+g++ -o bin/control src/control.cpp
+read -p "Done compiling! Run the program? (y/n): " answer
+if [ "${answer,,}" = "y" ]; then
+  echo
+  ./bin/control
+fi
+echo

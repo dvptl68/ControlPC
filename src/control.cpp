6405cd712ebd8c7602acabb812df04f3bfac2c3d
Dev <dvptl68@gmail.com>
2020-05-13 10:10:23 -0400

    Implement available drive finder

--- src/control.cpp
+++ src/control.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
-#include <cstdlib>
+#include <windows.h>
 
 using std::cout;
 using std::system;
 
+char lpBuffer[30];// buffer for drive string storage
 int main(){
-  cout << "hello";
+  DWORD test = GetLogicalDriveStrings( sizeof(lpBuffer), lpBuffer);
+  for(int i = 0; i<sizeof(lpBuffer); i++){
+    cout << lpBuffer[i];
+  }
+  return 0;
 }

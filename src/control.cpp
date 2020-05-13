869ff6d17410a49f0c7280502d3e0fad8f4a0a7a
Dev <dvptl68@gmail.com>
2020-05-13 10:51:08 -0400

    Implement drive list parser

--- src/control.cpp
+++ src/control.cpp
@@ -1,14 +1,30 @@
 #include <iostream>
 #include <windows.h>
+#include <vector>
+#include <string>
 
-using std::cout;
-using std::system;
+using namespace std;
 
-char lpBuffer[30];// buffer for drive string storage
 int main(){
+  //Buffer for drives
+  char lpBuffer[30];
+  //Fills buffer with drives
   DWORD test = GetLogicalDriveStrings( sizeof(lpBuffer), lpBuffer);
-  for(int i = 0; i<sizeof(lpBuffer); i++){
-    cout << lpBuffer[i];
+  //Parse buffer contents into a vector of drive names
+  vector<string> drives;
+  int i = 0;
+  while (i < (sizeof(lpBuffer)-2)){
+    if (lpBuffer[i+1] == ':' && (lpBuffer[i+2] == '\\' || lpBuffer[i+2] == '/')){
+      string current = "";
+      current += lpBuffer[i];
+      current += lpBuffer[i+1];
+      current += lpBuffer[i+2];
+      drives.push_back(current);
+    }
+    i++;
+  }
+  for (int i = 0; i < drives.size(); i++){
+    cout << drives[i] << ", ";
   }
   return 0;
 }

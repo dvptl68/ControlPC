5ddf3cb03abcd537fcc8e0ac4e929b9c28f82ea6
Dev <dvptl68@gmail.com>
2020-05-14 11:13:41 -0400

    Add drive name display

--- src/control.cpp
+++ src/control.cpp
@@ -1,63 +1,61 @@
 #include <iostream>
 #include <windows.h>
 #include <vector>
+#include <map>
 #include <string>
 
 using namespace std;
 
-vector<string> getDrives(){
+map<string, string> getDrives(){
   //Buffer for drives
   char lpBuffer[30];
   //Fills buffer with drives
-  DWORD test = GetLogicalDriveStrings( sizeof(lpBuffer), lpBuffer);
+  DWORD test = GetLogicalDriveStrings(sizeof(lpBuffer), lpBuffer);
   //Parse buffer contents into a vector of drive names
-  vector<string> drives;
+  map<string, string> drives;
   int i = 0;
   while (i < (sizeof(lpBuffer)-2)){
     //If the format of three consecutive characters is a letter + :\, add to drive vector
     if (lpBuffer[i+1] == ':' && lpBuffer[i+2] == '\\'){
-      string current = "";
-      current += lpBuffer[i];
-      current += lpBuffer[i+1];
-      current += lpBuffer[i+2];
+      char current[4] = {lpBuffer[i], ':', '\\'};
       //Filters out the boot drive
-      if (current != "C:\\"){
-        drives.push_back(current);
-      }
+      // if (current[0] != 'C'){
+        char VolumeName[MAX_PATH];
+        GetVolumeInformationA((LPCSTR)current, (LPSTR)VolumeName, sizeof(VolumeName), NULL, NULL, NULL, NULL, 0);
+        string letter(current);
+        string vol(VolumeName);
+        drives.insert(pair<string, string>(letter, vol));
+      // }
     }
     i++;
   }
   return drives;
 }
 
-string pickDrive(vector<string>& drives){
-  if (drives.size() > 0){
+string pickDrive(map<string, string>& drives){
+  if (!drives.empty()){
     //List all available drives
     cout << "Available drives: \n\n";
-    for (int i = 0; i < drives.size(); i++){
-      cout << (i+1) << ". " << drives[i] << "\n";
+    int i = 0;
+    for (const auto& x : drives) {
+      i++;
+      std::cout << i << ". " << x.first << " -> \"" << x.second << "\"\n";
     }
-    //Prompt user until a valid drive number is entered
-    int index = -1;
-    cout << "\nEnter the number for a drive: ";
-    cin >> index;
-    while (cin.fail() && (index < 1 || index > drives.size())){
-      cout << "You must enter a number between 1 and " << drives.size() << "!\n";
-      cout << "\nEnter the number for a drive: ";
-      cin.clear();
-      cin.ignore(256,'\n');
-      cin >> index;
-    }
-    string d = drives[index - 1];
-    cout << "You picked: " << d << "\n";
-    return d;
+    //Prompt user until a valid drive letter is entered
+    cout << "\nEnter the drive letter: ";
+    string letter;
+    cin >> letter;
+    letter += ":\\";
+    // string d = drives[index - 1];
+    // cout << "You picked: " << d << "\n";
+    return "";
   }else{
-    cout << "No available drives!";
+    cout << "No available drives!\n";
     return "";
   }
 }
 
 int main(){
-  vector<string> drives = getDrives();
-  pickDrive(drives);
+  map<string, string> drives = getDrives();
+  string d = pickDrive(drives);
 }

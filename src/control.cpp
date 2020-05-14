bed890df9c0a94da82855d53f1ab63ad5e6cb831
Dev <dvptl68@gmail.com>
2020-05-14 11:25:44 -0400

    Fix bug with drive input

--- src/control.cpp
+++ src/control.cpp
@@ -20,13 +20,13 @@ map<string, string> getDrives(){
     if (lpBuffer[i+1] == ':' && lpBuffer[i+2] == '\\'){
       char current[4] = {lpBuffer[i], ':', '\\'};
       //Filters out the boot drive
-      // if (current[0] != 'C'){
+      if (current[0] != 'C'){
         char VolumeName[MAX_PATH];
         GetVolumeInformationA((LPCSTR)current, (LPSTR)VolumeName, sizeof(VolumeName), NULL, NULL, NULL, NULL, 0);
         string letter(current);
         string vol(VolumeName);
         drives.insert({letter, vol});
-      // }
+      }
     }
     i++;
   }
@@ -42,16 +42,19 @@ string pickDrive(map<string, string>& drives){
       i++;
       std::cout << i << ". " << x.first << " -> \"" << x.second << "\"\n";
     }
-    //Prompt user until a valid drive letter is entered
+    //Prompt user to pick a drive by entering letter
     cout << "\nEnter the drive letter: ";
     string letter;
     cin >> letter;
+    //Convert to uppercase in case user entered lowercase letter
     transform(letter.begin(), letter.end(), letter.begin(), ::toupper);
     letter += ":\\";
+    //Use an iterator to find and print the selected drive from the map
     auto pos = drives.find(letter);
     cout << "You picked: " << pos->first << " - \"" << pos->second << "\"\n";
     return letter;
   }else{
+    //Notify the user if the only available drive is the boot drive
     cout << "No available drives!\n";
     return "";
   }

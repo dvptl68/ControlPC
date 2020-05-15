c27229e55f8a0e7f47631617b421c305c26246aa
Dev <dvptl68@gmail.com>
2020-05-15 14:26:10 -0400

    Fix bug with file display implementation

--- src/PickFile.cpp
+++ src/PickFile.cpp
@@ -10,34 +10,42 @@ using namespace std;
 
 //Utility function to get a new tree
 Tree newTree(string d){
+  //Create a new tree, set the data, and return the tree
   Tree t;
   t.data = d;
   return t;
 }
 
+//Traverses all the files on the drive and places filenames in tree structure
 void retrieveFiles(string& p, struct Tree& files){
   DIR *dir;
   struct dirent *e;
+  //Convert the path into a char array
   const char *path = p.c_str();
+  //Test if the given path is a directory
   if ((dir = opendir(path)) != NULL) {
-    vector<Tree> c;
+    //Populate the vector with filenames
     while ((e = readdir(dir)) != NULL) {
-      c.push_back(newTree(e->d_name));
+      files.children.push_back(newTree(e->d_name));
+      // string path = p + e->d_name + "\\";
+      // retrieveFiles(path, files.children[files.children.size()-1]);
     }
-    files.children = c;
     closedir (dir);
   }
 }
 
+//Constructor for PickFile class
 PickFile::PickFile(string p){
-  //Set the path private variable
+  //Set the path and name private variables
   PickFile::path = p.substr(0, 3);
   PickFile::name = p.substr(3);
+  //Set the Tree private variable
   PickFile::files = newTree(PickFile::path + " -> \"" + PickFile::name + "\"");
   //Fill the vector with all available files
   retrieveFiles(PickFile::path, PickFile::files);
 }
 
+//Returns a string with indentSize number of spaces
 string getIndent(int indentSize){
   string indent = "";
   for (int i = 0; i < indentSize; i++){
@@ -46,14 +54,18 @@ string getIndent(int indentSize){
   return indent;
 }
 
+//Traverses the filename tree and prints in orderly fashion
 void traversePrint(struct Tree f, int indentSize){
+  //Print data with number of indents preceding and make recursive call on each of the children
   cout << getIndent(indentSize) << f.data << "\n";
   for (int i = 0; i < f.children.size(); i++){
     traversePrint(f.children[i], indentSize + 2);
   }
 }
 
+//Uses helper functions to print all filenames to the user
 void PickFile::printFiles(){
+  //Print the title and call the recursive file print method
   cout << "\nAll available files and subdirectories in " << PickFile::path << " -> \"" << PickFile::name << "\": \n\n";
   traversePrint(PickFile::files, 0);
 }
--- src/Select.cpp
+++ src/Select.cpp
@@ -7,11 +7,13 @@
 
 using namespace std;
 
+//Constructor for Select class
 Select::Select(void){
   //Fills buffer with drives
   DWORD test = GetLogicalDriveStrings(sizeof(lpBuffer), lpBuffer);
 }
 
+//Parses the character buffer into strings with all drive paths
 void Select::parseBuffer(map<string, string>& drives){
   int i = 0;
   while (i < (sizeof(lpBuffer)-2)){
@@ -20,10 +22,12 @@ void Select::parseBuffer(map<string, string>& drives){
       char current[4] = {lpBuffer[i], ':', '\\'};
       //Filters out the boot drive
       if (current[0] != 'C'){
+        //Gets the name of the drive given the path
         char VolumeName[MAX_PATH];
         GetVolumeInformationA((LPCSTR)current, (LPSTR)VolumeName, sizeof(VolumeName), NULL, NULL, NULL, NULL, 0);
         string letter(current);
         string vol(VolumeName);
+        //Add path and drive name as a key-value pair into the map
         drives.insert({letter, vol});
       }
     }
@@ -31,6 +35,7 @@ void Select::parseBuffer(map<string, string>& drives){
   }
 }
 
+//Allows the user to pick a drive from the map
 string Select::pickDrive(map<string, string>& drives){
   if (!drives.empty()){
     //List all available drives

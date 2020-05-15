09f7af36ce2da5c7b5927e91ea9b15318b3278ec
Dev <dvptl68@gmail.com>
2020-05-15 09:24:38 -0400

    Update scripts to compile all class files

--- scripts/rBatch.cmd
+++ scripts/rBatch.cmd
@@ -2,7 +2,7 @@
 echo.
 echo Compiling...
 cd ..
-g++ -o bin/out src/encrypt.cpp src/Select.cpp
+g++ -o bin/out src/encrypt.cpp src/Select.cpp src/PickFile.cpp
 set /p answer="Done compiling! Run the program? (y/n): "
 if "%answer%"=="y" (
   echo.

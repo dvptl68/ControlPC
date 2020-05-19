b4c6f0bcf0814a00c469753fc354657e6e6c8cb5
Dev <dvptl68@gmail.com>
2020-05-19 13:32:58 -0400

    Improve overall structure

--- src/main.cpp
+++ src/main.cpp
@@ -5,72 +5,114 @@
 #include <map>
 #include <vector>
 #include <string>
-#include "DriveSelect.hpp"
-#include "GetFile.hpp"
-#include "FileData.hpp"
+#include "class/DriveSelect.hpp"
+#include "class/GetFile.hpp"
+#include "class/FileData.hpp"
 
 using namespace std;
 
-int main(){
-  //Create file for information storage
-  FileData d;
+//Verifies the user
+bool verify(FileData& d){
+
   string password;
+
   //Check if info file exists
   if (d.infoExists()){
+
     //If file exists, user has already created password and must verify it
     cout << "Enter the password: ";
     cin >> password;
+
     if (!d.checkPassword(password)){
+
       //End program if password is incorrect
       cout << "Wrong password!\n";
-      return 0;
+      return false;
     }
+
   }else{
+
     //Allow user to create a password
     cout << "Enter a password to be used for file encryption and decryption: ";
     cin >> password;
     d.setPassword(password);
   }
+
+  return true;
+}
+
+//Allows user to select a drive
+string pickDrive(){
+
   //Create class object for drive select
   DriveSelect s;
+
   //Create map to store drive letters and their names
   map<string, string> drives;
+
   //Fill map with parsed drive letters and names
   s.parseBuffer(drives);
+
   //Get drive letter from user
-  string dr = s.pickDrive(drives);
+  return s.pickDrive(drives);
+}
+
+//Driver function if encrypt is selected
+void encryptSelected (FileData& d, string& dr){
+
+  //Create class object for file select and report the time it takes
+  cout << "\nParsing file structure...";
+  clock_t start = clock();
+  GetFile f(dr, true);
+  cout << "completed in " << (std::clock() - start) / (double) CLOCKS_PER_SEC << " seconds.";
+
+  //Print all available files
+  f.printFiles();
+
+  //Get and print all files that are already encrypted
+  vector<string> encrypted = d.getFileInfo(dr);
+
+  //Allow user to select files to encrypt
+  vector<string> files = f.pickEncryptFiles(encrypted);
+
+  //encrypt files
+
+  //Add selected files to info file
+  d.addFileInfo(files);
+}
+
+//Driver function if decrypt is selected
+void decryptSelected(FileData& d, string& dr){
+
+  //Get and print all files that are already encrypted
+  vector<string> encrypted = d.getFileInfo(dr);
+
+  //Allow user to select files to decrypt
+  GetFile f(dr, false);
+  vector<string> decrypt = f.pickDecryptFiles(encrypted);
+
+  //decrypt files
+
+  d.removeFileInfo(encrypted, decrypt);
+}
+
+int main(){
+
+  //Create file for information storage
+  FileData d;
+  //End the program if user verification fails
+  if (!verify(d)) { return 0; }
+  
+  //Get a drive path from user
+  string dr = pickDrive();
   //End program if there are no available drives
-  if (dr.length() == 0){ return 0; }
+  if (dr.length() == 0) { return 0; }
+
   //Allow user to choose whether to encrypt or decrypt files
   int choice;
   cout << "\nEnter 1 to encrypt files or 2 to decrypt files: ";
   cin >> choice;
-  if (choice == 1){
-    //Create class object for file select and report the time it takes
-    cout << "\nParsing file structure...";
-    clock_t start = clock();
-    GetFile f(dr, true);
-    cout << "completed in " << (std::clock() - start) / (double) CLOCKS_PER_SEC << " seconds.";
-    //Print all available files
-    f.printFiles();
-    //Get and print all files that are already encrypted
-    vector<string> encrypted = d.getFileInfo(dr);
-    //Allow user to select files
-    vector<string> files = f.pickEncryptFiles(encrypted);
 
-    //encrypt files
-
-    //Add selected files to info file
-    d.addFileInfo(files);
-  }else if (choice == 2){
-    //Get and print all files that are already encrypted
-    vector<string> encrypted = d.getFileInfo(dr);
-    //Allow user to select files
-    GetFile f(dr, false);
-    vector<string> decrypt = f.pickDecryptFiles(encrypted);
-
-    //decrypt files
-
-    d.removeFileInfo(encrypted, decrypt);
-  }
+  //Calls the appropriate driver function based on user selection
+  if (choice == 1) { encryptSelected(d, dr); } else if (choice == 2) { decryptSelected(d, dr); } else { return 0; }
 }

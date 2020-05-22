7c74c07ae4ed616e25d75503ca558bb7e9622577
Dev <dvptl68@gmail.com>
2020-05-22 07:36:22 -0400

    Implement decryption algorithm

--- src/class/EFile.cpp
+++ src/class/EFile.cpp
@@ -46,4 +46,34 @@ void EFile::encrypt(string& path){
 
 //Decrypts the file at the given path using the key
 void EFile::decrypt(string& path){
+  //Open source file as input stream and temp file as output stream
+  ifstream fileIn(path);
+  ofstream tempOut("temp.txt");
+  //Read each line from file until end is reached
+  string p;
+  while(getline(fileIn, p)){
+    //Iterate through line decrypting each character
+		for (int i = 0; i < p.length(); i++){
+      p[i] = p[i] - 1;
+    }
+    //Add encrypted line to temp file
+		tempOut << p << endl;
+	}
+  //Close all open files
+  fileIn.close();
+  tempOut.close();
+  //Open source file as output stream and temp file as input stream
+  ofstream fileOut(path);
+  ifstream tempIn("temp.txt");
+  //Read each line from temp file until end is reached
+  string t;
+  while(getline(tempIn, t)){
+    //Add encrypted line back to source file
+		fileOut << t << endl;
+	}
+  //Close all open files
+  fileOut.close();
+  tempIn.close();
+  //Delete temp file
+  remove("temp.txt");
 }
